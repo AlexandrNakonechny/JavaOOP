6b5d21568ad04672943ec04898bd756397a22628
AlexandrNakonechny <44878021+AlexandrNakonechny@users.noreply.github.com>
2018-11-13 15:41:13 +0200

    Update Main.java

--- Lesson2HomeWork/Main.java
+++ Lesson2HomeWork/Main.java
@@ -3,7 +3,7 @@ package JavaOOP.Lesson2HomeWork;
 public class Main {
     public static void main(String[] args) {
         Point centerCircle = new Point(3, 2);
-        Point pointCircle = new Point(8,11);
+        Point pointCircle = new Point(8, 11);
 
         Point pointTriA = new Point(0, 0);
         Point pointTriB = new Point(0, 4);
@@ -15,24 +15,26 @@ public class Main {
         Point pointQuaD = new Point(6, 0);
 
         Circle circle = new Circle(centerCircle, pointCircle);
-        System.out.println(circle.getArea());
-        System.out.println(circle.getPerimeter());
+        System.out.println("Area of circle is: " + circle.getArea());
+        System.out.println("Perimeter of circle is: " + circle.getPerimeter());
         System.out.println();
 
         Triangle triangle = new Triangle(pointTriA, pointTriB, pointTriC);
-        System.out.println(triangle.getArea());
-        System.out.println(triangle.getPerimeter());
+        System.out.println("Area of triangle is: " + triangle.getArea());
+        System.out.println("Perimeter of triangle is: " + triangle.getPerimeter());
         System.out.println();
 
         Quadrangle quadrangle = new Quadrangle(pointQuaA, pointQuaB, pointQuaC, pointQuaD);
-        System.out.println(quadrangle.getPerimeter());
-        System.out.println(quadrangle.getArea());
+        System.out.println("Perimeter of quadrangle is: " + quadrangle.getPerimeter());
+        System.out.println("Area of quadrangle is: " + quadrangle.getArea());
         System.out.println();
 
         Table table = new Table();
         table.addShape(0, circle);
         table.addShape(2, triangle);
         table.addShape(3, quadrangle);
+        System.out.println("Sum areas of all shapes is: " + table.getSumAreasOfAllShapes());
+        System.out.println();
         System.out.println(table.showAllShapes());
 
         table.deleteShape(2);
